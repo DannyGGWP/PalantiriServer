ab11b69df3b59b35a409447dda1f329917b48206
Daniel Guerrera <dan.guerrera@gmail.com>
2020-02-06 10:35:07 -0500

    switching to mysql

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
-var msg = "Hello World";
-console.log(msg); 
+const MongoClient = require('mongodb').MongoClient; 
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017/scoutingdb';
+
+MongoClient.connect(url, function(err,db){
+    if (err) throw err;
+    console.log('Database Created');
+    db.close();
+})
